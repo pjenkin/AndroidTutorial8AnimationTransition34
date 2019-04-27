29c12dad9cc439cbc0386dd7d89ab6b63265a8cf
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-27 18:16:21 +0100

    34 In ConstraintLayout, button repositioned & resized on click of layout

--- app/src/main/java/com/example/animationtransit34/MainActivity.java
+++ app/src/main/java/com/example/animationtransit34/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.animationtransit34;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewGroup pnjLayout;     // NB ConstraintLayout and RelativeLayout subclasses of ViewGroup
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        pnjLayout = (ViewGroup) findViewById(R.id.pnj_layout);
+
+        pnjLayout.setOnTouchListener(
+                new ConstraintLayout.OnTouchListener()      // oops. NB capital case again!
+                {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+//                        return false;
+                        moveButton();       // sensibly, use new bespoke method/function
+                        return true;        // true: 'touch' event handled
+                    }
+                }
+                );
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +47,44 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    /// button in corner, and bigger, with animation
+    public void moveButton()
+    {
+        View pnjButton = findViewById(R.id.pnj_button);
+        View pnjConstraintLayout = findViewById(R.id.pnj_layout);
+
+        // 1) reposition button 2) resize button
+
+        // video's code using RelativeLayout
+//        ConstraintLayout.LayoutParams positionRules = new ConstraintLayout.LayoutParams(
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//        // width & height
+//
+//        positionRules.addRule(ConstraintLayout.TEXT_ALIGNMENT_TEXT_END, ConstraintLayout.TR);
+//        positionRules.addRule(ConstraintLayout.TEXT_ALIGNMENT_TEXT_END);
+
+
+        // if using new-fangled ConstraintLayout instead of RelativeLayout
+        // 1) Reposition button
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.pnj_layout);
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+        constraintSet.connect(R.id.pnj_button, ConstraintSet.BOTTOM, R.id.pnj_layout, ConstraintSet.BOTTOM);
+        // stick bottom of button to bottom of layout
+        constraintSet.connect(R.id.pnj_button, ConstraintSet.RIGHT, R.id.pnj_layout, ConstraintSet.RIGHT);
+        // stick right of button to right of layout
+        constraintSet.applyTo(constraintLayout);
+
+        // 2) Resize button
+        ViewGroup.LayoutParams sizeRules = pnjButton.getLayoutParams();
+        sizeRules.width = 450;
+        sizeRules.height = 300;
+        pnjButton.setLayoutParams(sizeRules);
+
     }
 
     @Override
